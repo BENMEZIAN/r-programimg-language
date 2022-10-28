a4ce5e32781903a56604187514f5f6469c1ad5e4
BENMEZIAN <44295887+BENMEZIAN@users.noreply.github.com>
2022-10-28 23:18:44 +0100

    Update test.R

--- test.R
+++ test.R
@@ -1,19 +1,34 @@
+x <- 10
+y <- 25
+z <- sum(x,y)
+
+cat("x + y = ", z,"\n")    # using new lines
+cat("GEEKS \nFOR \nGEEKS\n") # using new lines
+print("----------------------------------\n")
 name <- "John"
-class(name)
+cat("your name is:", name,"\n")
+cat("type of name is:",class(name),"\n")
+print("----------------------------------\n")
 age <- 40
-class(age)
+cat("your age is:", age,"\n")
+cat("type of age is:",class(age),"\n")
+print("---------------------------------\n")
 salary <- 65000.0
-class(salary)
-name   # output "John"
-age    # output 40
-salary # output 65000.0
+cat("your salary is:", salary,"\n")
+cat("type of salary is:",class(salary),"\n")
+print("---------------------------------\n")
+x <- 1000L
+cat("your x is:", x,"\n")
+cat("type of x is:",class(x),"\n")
+print("---------------------------------\n")
+logic <- TRUE
+cat("your name is:", logic,"\n")
+cat("type of name is:",class(logic),"\n")   # print without quotes
+sprintf("type of name is: %s",class(logic)) # print with quotes
+print("---------------------------------\n")
 
 text <- "awesome"
-str <- paste("R is", text) # concatenation
-class(str)
-num1 <- 10
-num2 <- 20
+cat(paste("R is", text,"\n")) # concatenation using paste
 
-num1+num2
 
-num1+text
+
