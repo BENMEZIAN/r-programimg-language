96ad8d7fc56affc5ddefd4660c45a01288941187
BENMEZIAN <44295887+BENMEZIAN@users.noreply.github.com>
2022-10-28 23:18:00 +0100

    Add files via upload

---
+++ test.R
@@ -0,0 +1,19 @@
+name <- "John"
+class(name)
+age <- 40
+class(age)
+salary <- 65000.0
+class(salary)
+name   # output "John"
+age    # output 40
+salary # output 65000.0
+
+text <- "awesome"
+str <- paste("R is", text) # concatenation
+class(str)
+num1 <- 10
+num2 <- 20
+
+num1+num2
+
+num1+text
